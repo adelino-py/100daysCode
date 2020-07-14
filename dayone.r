11aa80a91fb3facfc794bf965ed223fffe12cd25
adelino-py <53866538+adelino-py@users.noreply.github.com>
2020-07-13 23:20:18 -0300

    Create dayone.r
    Modulo 1 - Introduction to R (DataCamp)

---
+++ dayone.r
@@ -0,0 +1,91 @@
+# Calculate 3 + 4
+3 + 4
+
+# Calculate 6 + 12
+6 + 12
+
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+# A division 
+(5 + 5) / 2 
+
+# Exponentiation
+2^5
+
+# Modulo
+28%%6
+
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+print (x)
+
+# Assign the value 5 to the variable my_apples
+my_apples <- 5 
+
+# Print out the value of the variable my_apples
+print (my_apples)
+
+# Assign a value to the variable my_apples
+my_apples <- 5 
+
+# Fix the assignment of my_oranges
+my_oranges <- 6
+
+# Create the variable my_fruit and print it out
+my_fruit <- my_apples + my_oranges 
+print (my_fruit)
+
+# Change my_numeric to be 42
+my_numeric <- 42
+
+
+# Change my_character to be "universe"
+my_character <- "universe"
+
+# Change my_logical to be FALSE
+my_logical <- FALSE
+
+# Declare variables of different types
+my_numeric <- 42
+my_character <- "universe"
+my_logical <- FALSE 
+
+# Check class of my_numeric
+class(my_numeric)
+# Check class of my_character
+class(my_character)
+
+# In R, you create a vector with the combine function c(). 
+#You place the vector elements separated by a comma between the parentheses. 
+#For example:
+
+numeric_vector <- c(1, 10, 49)
+character_vector <- c("a", "b", "c")
+
+#Complete the code such that boolean_vector contains the three elements: TRUE, FALSE and TRUE (in that order).
+boolean_vector <- c(TRUE, FALSE, TRUE)
+
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
+
+# Assign days as names of poker_vector
+names(poker_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+# The variable days_vector
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+ 
+# Assign the names of the day to roulette_vector and poker_vector
+names(poker_vector) <- c(days_vector)
+names(roulette_vector) <- c(days_vector)
