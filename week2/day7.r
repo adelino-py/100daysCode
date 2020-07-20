1ac27031966b6ad3edc828b73d7c8930199d36ce
Mariana <araujomariana34@gmail.com>
2020-07-20 02:52:49 -0300

    Introduction to R

--- week2/day7.r
+++ week2/day7.r
@@ -1,1 +1,29 @@
+# Data Frames
+#head() and tail()
 
+head(mtcars) #first observations
+tail(mtcars) #last observations
+#hp: potência - datsun menor
+
+#structure - str()
+str(mtcars)
+
+# Creating a data frame
+# Definition of vectors
+name <- c("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+type <- c("Terrestrial planet", "Terrestrial planet", "Terrestrial planet", "Terrestrial planet", "Gas giant", "Gas giant", "Gas giant", "Gas giant")
+diameter <- c(0.382, 0.949, 1, 0.532, 11.209, 9.449, 4.007, 3.883)
+rotation <- c(58.64, -243.02, 1, 1.03, 0.41, 0.43, -0.72, 0.67)
+rings <- c(FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE)
+
+# Create a data frame from the vectors
+planets_df <- data.frame(name, type, diameter, rotation, rings)
+
+# Print out diameter of Mercury (row 1, column 3)
+planets_df[1,3]
+
+# Print out data for Mars (entire fourth row)
+planets_df[4,]
+
+# Select first 5 values of diameter column
+planets_df[1:5,"diameter"]
