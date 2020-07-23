33d41d199d6beb40ceef862629ca0694229d430f
Mariana <araujomariana34@gmail.com>
2020-07-22 23:39:09 -0300

    Introduction to Import Data in R

---
+++ Introduction to Importing Data in R/week2/day10.r
@@ -0,0 +1,7 @@
+# http://rstudio-pubs-static.s3.amazonaws.com/478659_26b84af940d04ea3b4f564f47e47502e.html
+library(readr)
+
+# Column names
+properties <- c("area", "temp", "size", "storage", "method", "texture", "flavor", "moistness")
+potatoes <- read.tsv("http://s3.amazonaws.com/assets.datacamp.com/production/course_1477/datasets/potatoes.txt", header = FALSE)
+print (head(potatoes))
--- Introduction to Importing Data in R/week2/day9.r
+++ Introduction to Importing Data in R/week2/day9.r
@@ -9,13 +9,20 @@ pool <- read.csv ("swimming_pools.csv", stringsAsfactors = TRUE)
 
 ## exotic file format read.table any yabular file as a data frame
 # header - primeira linha é nome de variaveis
-# txt read.delim
-
-hotdogs <- read.delim("http://s3.amazonaws.com/assets.datacamp.com/production/course_1477/datasets/hotdogs.txt", header = FALSE)
-str(hotdogs)
 
 # or
-#hotdogs <- read.table(path, sep = "\t", col.names = c("type", "calories", "sodium"))
+#hotdogs <- read.table
+# read.table header é false
 
+# txt read.delim
+hotdogs <- read.delim("http://s3.amazonaws.com/assets.datacamp.com/production/course_1477/datasets/hotdogs.txt", header = FALSE, col.names = c("type", "calories", "sodium"))
+print (str(hotdogs))
 
+# Select the hot dog with the least calories: lily
+lily <- hotdogs[which.min(hotdogs$calories), ]
 
+# Select the observation with the most sodium: tom
+tom <- hotdogs[which.max(hotdogs$sodium), ]
+
+hotdogs2 <- read.delim("http://s3.amazonaws.com/assets.datacamp.com/production/course_1477/datasets/hotdogs.txt", header = FALSE, col.names = c("type", "calories", "sodium"), colClasses = c("factor", "NULL","numeric"))
+print (str(hotdogs2))
